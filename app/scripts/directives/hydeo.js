43d5dcf6c21c614876828315a7be5506f697fa77
centsent <ivan.hui.g+d@gmail.com>
2015-09-08 22:53:12 +0800

    added hydeo directive

--- app/scripts/directives/hydeo.js
+++ app/scripts/directives/hydeo.js
@@ -1,40 +1,116 @@
+/**
+ *
+ * hypeo derective, used to playing html5 video base on Videogular.
+ *
+ */
 (function() {
+
   var app = angular.module('hydeo', [
     'ngSanitize',
     'com.2fdevs.videogular',
-    'com.2fdevs.videogular.plugins.controls'
+    'com.2fdevs.videogular.plugins.controls',
+    'uk.ac.soton.ecs.videogular.plugins.cuepoints'
   ]);
+
+  /**
+   * Simplified and transform the vg-cue-points configurations.
+   */
+  function toCuePoint(cuepointList, api) {
+    if(!cuepointList || !cuepointList.length) {
+      return ;
+    }
+
+    var result = {
+      list: []
+    };
+
+    _.forEach(cuepointList, function(cp) {
+      if(cp) {
+        var cuepoint = {};
+        var timeLapse = {
+          start: cp.time
+        };
+
+        if(_.isPlainObject(cp.time)) {
+          timeLapse = cp.time;
+        }
+
+        cuepoint = {
+          timeLapse: timeLapse,
+          // mapping onEnter to onUpdate
+          onUpdate: function onEnter(currentTime, timeLapse, params) {
+            if(_.isFunction(cp.onEnter)) {
+              cp.onEnter(currentTime, timeLapse, api, params);
+            }
+          },
+          // mapping onLeave to onComplete
+          onComplete: function onLeave(currentTime, timeLapse, params) {
+            if(_.isFunction(cp.onLeave)) {
+              cp.onLeave(currentTime, timeLapse, api, params);
+            }
+          },
+          params: cp.params
+        };
+
+        result.list.push(cuepoint);
+      }
+    });
+
+    return result;
+  }
 
   app.directive('hydeo', ['$sce', function($sce) {
     return {
+      // only work on Element.
       resctrict: 'E',
-      templateUrl: 'views/directives/hydeo.html',
+
+      templateUrl: 'scripts/directives/hydeo.html',
+
       scope: {
-        options: '='
+
+        /**
+         * Object containing a list of timelines with cue points. Each property in the object represents a timeline, which is an Array of objects with the next definition
+         * <pre>
+         * {
+         *  time: Define in seconds when this timeline is active, can be an integer or an Object with start and end properties.
+         *  onEnter: Callback function that will be called when timeline reach to the time property.
+         *  onUpdate: Callback function that will be called when the progress is in the time property.
+         *  onLeave: Callback function that will be called when the progress is over the time property.
+         *  params: Custom object with data to pass to the callbacks.
+         * }
+         * </pre>
+         */
+        cuepoints: '=',
+
+        /**
+         * Video source url
+         */
+        src: '='
       },
-      link: function ($scope, elem, attr, API) {
+
+      link: function ($scope, elem, attr) {
+        // TODO configurable event
+        $scope.onPlayerReady = function onPlayerReady(api) {
+          $scope.api = api;
+        };
+
         $scope.config = {
           sources: [{
-            src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4'),
+            src: $sce.trustAsResourceUrl($scope.src),
+            // TODO configurable media type
             type: 'video/mp4'
           }],
-          tracks: [{
-            src: 'http://www.videogular.com/assets/subs/pale-blue-dot.vtt',
-            kind: 'subtitles',
-            srclang: 'en',
-            label: 'English',
-            'default': ''
-          }],
+          // TODO styling a default theme
           theme: 'bower_components/videogular-themes-default/videogular.css',
+          cuePoints: toCuePoint($scope.cuepoints, api),
           plugins: {
+            // TODO more controls & plugins
             cuepoints: {
+              // TODO styling a default theme
               theme: {
                 url: 'bower_components/videogular-cuepoints/cuepoints.css'
               },
-              points: [
-                {time: 18},
-                {time: 60}
-              ]
+              points: $scope.cuepoints
             }
           }
         };
