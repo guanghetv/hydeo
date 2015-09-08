f0b1f8a7a2644fcf265991d0894cc0e8505767e4
centsent <ivan.hui.g+d@gmail.com>
2015-09-08 22:53:44 +0800

    simple hydeo demo

--- app/index.js
+++ app/index.js
@@ -1,6 +1,21 @@
 (function(){
-  angular.module('hydeoApp', ['hydeo']).controller(function() {
 
+  // Simple hydeo demo.
+  angular.module('hydeoApp', ['hydeo']).controller('HomeController', function($sce) {
+    var _this = this;
+
+    _this.cuepoints = [{
+      time: 10,
+      onLeave: function onLeave(currentTime) {
+        console.log(parseInt(currentTime, 10));
+      },
+      onEnter: function onComplete(currentTime, timeLapse) {
+        console.log('enter:' + parseInt(currentTime, 10));
+      }
+    }];
+
+    _this.src = 'http://static.videogular.com/assets/videos/videogular.mp4';
   });
+
 })();
 
