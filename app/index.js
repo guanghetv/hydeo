ad15ba0602563eed3d7329dc48bce59a365e20a3
centsent <ivan.hui.g+d@gmail.com>
2015-09-14 12:21:10 +0800

    added a video controlling service

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@
     _this.cuepoints = [{
       time: 10,
       templateUrl: 'views/quiz.html'
-    }];
+    },{time:20}];
 
     _this.src = 'http://7xaw4c.com2.z0.glb.qiniucdn.com/有理数_3a_有理数的乘法异号.mp4';
   }).controller('QuizController', [function() {
