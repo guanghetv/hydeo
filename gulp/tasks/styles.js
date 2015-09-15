02f952281c795f80c7e2c423ad0cf0653320c317
centsent <ivan.hui.g+d@gmail.com>
2015-09-15 15:49:19 +0800

    fixed styles task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,13 +1,16 @@
-import config       from '../config';
-import gulp         from 'gulp';
-import handleErrors from '../util/handleErrors';
-import browserSync  from 'browser-sync';
-import autoprefixer from 'gulp-autoprefixer';
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 
 gulp.task('styles', () => {
-  return gulp.src(config.styles.src)
-    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
-    .on('error', handleErrors)
-    .pipe(gulp.dest(config.styles.dest))
-    .pipe(browserSync.stream({ once: true }));
+  return gulp.src('app/styles/*.css')
+    .pipe($.sourcemaps.init())
+    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('build/styles'))
+    .pipe(reload({stream: true}));
 });
+
