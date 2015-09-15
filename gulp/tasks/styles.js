409d8f72cdc65a6048f5132153d2f2017fa1b0e5
centsent <ivan.hui.g+d@gmail.com>
2015-09-15 11:37:43 +0800

    fixed styles task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,13 @@
 import config       from '../config';
 import gulp         from 'gulp';
-import gulpif       from 'gulp-if';
-import sourcemaps   from 'gulp-sourcemaps';
 import handleErrors from '../util/handleErrors';
 import browserSync  from 'browser-sync';
 import autoprefixer from 'gulp-autoprefixer';
 
 gulp.task('styles', () => {
-
   return gulp.src(config.styles.src)
     .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
     .on('error', handleErrors)
     .pipe(gulp.dest(config.styles.dest))
     .pipe(browserSync.stream({ once: true }));
-
 });
