453cca347fefb9f6d33989bbb7f68397a678065f
centsent <ivan.hui.g+d@gmail.com>
2015-09-21 18:14:46 +0800

    added hydeo control service to control the meida

--- app/scripts/services/hydeo_control.js
+++ app/scripts/services/hydeo_control.js
@@ -1,7 +1,49 @@
 import servicesModule from './_index';
 
 class HydeoControl {
+  setApi(api) {
+    if(!this.api) {
+      this.api = api;
+    }
+  }
+
+  pause() {
+    if(!this.isPause()) {
+      this.api.pause();
+    }
+  }
+
+  play() {
+    if(!this.isPlay()) {
+      this.api.play();
+    }
+  }
+
+  stop() {
+    if(!this.isStop()) {
+      this.api.stop();
+    }
+  }
+
+  isPause() {
+    return this.api.currentState === 'pause';
+  }
+
+  isPlay() {
+   return this.api.currentState === 'play';
+  }
+
+  isStop() {
+    return this.api.currentState === 'stop';
+  }
+
+  get currentTime() {
+    return this.api.currentTime;
+  }
 }
 
-servicesModule.factory('$hydeoControl', HydeoControl);
+function $hydeoControl() {
+  return new HydeoControl();
+}
 
+servicesModule.factory('$hydeoControl', $hydeoControl);
