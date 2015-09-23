4115de2a46235ec7230f867ebc763d2791c82adf
centsent <ivan.hui.g+d@gmail.com>
2015-09-23 18:52:52 +0800

    modified views task, removed angular template cache

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -6,15 +6,16 @@ import templateCache from 'gulp-angular-templatecache';
 // Views task
 gulp.task('views', () => {
   // Put our index.html in the dist folder
-  gulp.src('app/index.html')
-    .pipe(gulp.dest(config.dist.root));
-
+  //gulp.src('app/index.html')
+    //.pipe(gulp.dest(config.dist.root));
   // Process any other view files from app/views
-  return gulp.src(config.views.src)
-    .pipe(templateCache({
-      standalone: true
-    }))
-    .pipe(gulp.dest(config.views.dest))
-    .pipe(browserSync.stream({ once: true }));
-
+/*  return gulp.src(config.views.src)*/
+    //.pipe(templateCache({
+      //standalone: true
+    //}))
+    //.pipe(gulp.dest(config.views.dest))
+    //.pipe(browserSync.stream({ once: true }));
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest(config.dist.root))
+    .pipe(browserSync.stream({once: true}));
 });
