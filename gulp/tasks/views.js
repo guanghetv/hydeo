9dd3ca05d4f0935985d8cfc3b9bb8329718039fa
centsent <ivan.hui.g+d@gmail.com>
2015-09-24 17:49:04 +0800

    modified views task

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -1,21 +1,10 @@
 import config from '../config';
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
-import templateCache from 'gulp-angular-templatecache';
 
 // Views task
 gulp.task('views', () => {
-  // Put our index.html in the dist folder
-  //gulp.src('app/index.html')
-    //.pipe(gulp.dest(config.dist.root));
-  // Process any other view files from app/views
-/*  return gulp.src(config.views.src)*/
-    //.pipe(templateCache({
-      //standalone: true
-    //}))
-    //.pipe(gulp.dest(config.views.dest))
-    //.pipe(browserSync.stream({ once: true }));
   return gulp.src('app/**/*.html')
     .pipe(gulp.dest(config.dist.root))
-    .pipe(browserSync.stream({once: true}));
+    .pipe(browserSync.stream());
 });
