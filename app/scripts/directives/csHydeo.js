5679aece1b750426ee072770d97eb9c4bbb05e48
centsent <ivan.hui.g+d@gmail.com>
2015-09-30 08:31:22 +0800

    define hydeo directive in a class

--- app/scripts/directives/csHydeo.js
+++ app/scripts/directives/csHydeo.js
@@ -3,21 +3,26 @@ import template from '../../views/directives/csHydeo.html';
 
 /**
  *
- * Do not use ES6 class to define a directive, because,
- * the link function is not called in the context of the directive object.
- * So, this reference inside the link function is not same as the directive object.
- *
  */
-function hydeoDirective() {
-  return {
-    restrict: 'E',
-    template: template,
-    scope: {
+class HydeoDirective {
+
+  constructor() {
+    this.restrict = 'E';
+    this.template = template;
+    this.controller = 'hydeoController';
+    this.scope = {
       cuepoints: '=',
       src: '='
-    },
-    controller: 'hydeoController'
-  };
+    };
+  }
+
+  /**
+   *
+   */
+  static factory() {
+    return new HydeoDirective();
+  }
+
 }
 
-directivesModule.directive('csHydeo', hydeoDirective);
+directivesModule.directive('csHydeo', HydeoDirective.factory);
