72336682fa8eb1161a71c85dc9bf855348535c7c
centsent <ivan.hui.g+d@gmail.com>
2015-10-09 15:25:35 +0800

    proxy on vgCuepoints

--- app/scripts/controllers/hydeoController.js
+++ app/scripts/controllers/hydeoController.js
@@ -3,7 +3,6 @@ import controllersModuel from './_index';
 const _scope = new WeakMap();
 const _sce = new WeakMap();
 const _hyMedia = new WeakMap();
-const _api = new WeakMap();
 
 /**
  *
@@ -52,19 +51,14 @@ class HydeoController {
   onPlayerReady(api) {
     const $hyMedia = _hyMedia.get(this);
     $hyMedia.setApi(api);
-    _api.set(this, api);
   }
 
   /**
    *
    */
-  calcLeft(point) {
-    if (!this.$$api || this.$$api.totalTime === 0) {
-      // Don't show the cuepoints if video not ready.
-      return '-1000';
-    }
-    const videoLength = this.$$api.totalTime / 1000;
-    return (point.time * 100 / videoLength).toString();
+  set vgCuepoints(cuepoints) {
+    const $scope = _scope.get(this);
+    $scope.config.cuePoints = cuepoints;
   }
 
 }
