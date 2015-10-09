158aea18951ffde96f19b67047223e193d16bbb3
centsent <ivan.hui.g+d@gmail.com>
2015-10-09 15:27:49 +0800

    writing hyCuepoint directive in es6

--- app/scripts/directives/hyCuepoint.js
+++ app/scripts/directives/hyCuepoint.js
@@ -3,7 +3,11 @@ import template from '../../views/directives/hyCuepoint.html';
 
 const _scope = new WeakMap();
 const _hyMedia = new WeakMap();
+const _hydeoController = new WeakMap();
 
+/**
+ *
+ */
 class CuepointDirective {
 
   constructor($hyMedia) {
@@ -17,30 +21,120 @@ class CuepointDirective {
     _hyMedia.set(this, $hyMedia);
   }
 
+  /**
+   *
+   */
   compile() {
     return this.link.bind(this);
   }
 
-  link($scope) {
+  /**
+   *
+   */
+  link($scope, elem, attr, hydeoController) {
     _scope.set(this, $scope);
+    _hydeoController.set(this, hydeoController);
     this.binding();
+    this.toVgCuepoints($scope.cuepoints);
   }
 
+  /**
+   *
+   */
   binding() {
     const $scope = _scope.get(this);
     $scope.styling = this.styling.bind(this);
   }
 
+  /**
+   *
+   */
   styling(point) {
     const $hyMedia = _hyMedia.get(this);
+    const totalTime = $hyMedia.totalTime;
+
+    if (totalTime === 0) {
+      return null;
+    }
+
     const style = {};
-    const totalTime = $hyMedia.totalTime();
-    // TODO deal with if totalTime is 0
+    // Convert millisecond to second.
     const videoLength = totalTime / 1000;
-    const left = `${point.time * 100 / videoLength}%`;
+    const left = `${point.time / videoLength * 100}%`;
     style.left = left;
 
     return style;
+  }
+
+  /**
+   *
+   */
+  onEnter(cuepoint) {
+    const $hyMedia = _hyMedia.get(this);
+    const tempOnEnter = cuepoint.onEnter;
+    this.current = this.current || cuepoint;
+    cuepoint.timeLapse = {
+      start: cuepoint.time
+    };
+
+    cuepoint.onEnter = (currentTime, timeLapse, params) => {
+      $hyMedia.pause();
+
+      if (typeof tempOnEnter === 'function') {
+        tempOnEnter(currentTime, timeLapse, params);
+      }
+    };
+  }
+
+  /**
+   *
+   */
+  onComplete(cuepoint) {
+    const $hyMedia = _hyMedia.get(this);
+    const tempOnComplete = cuepoint.onComplete;
+
+    cuepoint.onComplete = (currentTime, timeLapse, params) => {
+      $hyMedia.play();
+
+      if (typeof tempOnComplete === 'function') {
+        tempOnComplete(currentTime, timeLapse, params);
+      }
+    };
+  }
+
+  /**
+   *
+   */
+  onLeave() {}
+
+  /**
+   *
+   */
+  onUpdate() {}
+
+  /**
+   *
+   */
+  toVgCuepoints(cuepoints) {
+    if (!cuepoints) {
+      return;
+    }
+
+    const hydeoController = _hydeoController.get(this);
+    const result = {
+      list: []
+    };
+
+    cuepoints.forEach((cuepoint) => {
+      this.onEnter(cuepoint);
+      this.onComplete(cuepoint);
+      this.onLeave(cuepoint);
+      this.onUpdate(cuepoint);
+
+      result.list.push(cuepoint);
+    });
+
+    hydeoController.vgCuepoints = result;
   }
 
   /**
