3c72917d939beb22420a0ad1acc637f02627e0b7
centsent <ivan.hui.g+d@gmail.com>
2015-10-10 18:41:07 +0800

    added hy-overlay directive

---
+++ app/scripts/directives/hyOverlay.js
@@ -0,0 +1,46 @@
+import directivesModule from './_index';
+import template from '../../views/directives/hyOverlay.html';
+
+const _scope = new WeakMap();
+
+/**
+ *
+ */
+class HyOverlayDirective {
+
+  constructor() {
+    this.restrict = 'E';
+    this.template = template;
+    this.require = '^hyHydeo';
+    this.scope = {
+      templateUrl: '='
+    };
+  }
+
+  /**
+   *
+   */
+  compile() {
+    return this::this.link;
+  }
+
+  /**
+   *
+   */
+  link($scope) {
+    _scope.set(this, $scope);
+    this.binding();
+  }
+
+  /**
+   *
+   */
+  binding() {
+  }
+
+  static factory() {
+    return new HyOverlayDirective();
+  }
+}
+
+directivesModule.directive('hyOverlay', HyOverlayDirective.factory);
