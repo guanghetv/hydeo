518079d40fe00de78f7c22ca414b65cb10e45760
centsent <ivan.hui.g+d@gmail.com>
2015-10-28 23:40:36 +0800

    chore: remove angular template cache

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -1,14 +1,10 @@
 import config from '../config';
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
-import templateCache from 'gulp-angular-templatecache';
 
 // Views task
 gulp.task('views', () => {
   return gulp.src(config.views.src)
-    .pipe(templateCache({
-      standalone: true
-    }))
     .pipe(gulp.dest(config.views.dest))
     .pipe(browserSync.stream());
 });
