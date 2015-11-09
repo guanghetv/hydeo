5c78752f2c9a78d62d8cc91aa251157523f217af
centsent <ivan.hui.g+d@gmail.com>
2015-11-10 02:03:58 +0800

    chore: fix karma config file path problem

--- gulp/tasks/unit.js
+++ gulp/tasks/unit.js
@@ -1,10 +1,11 @@
 import gulp from 'gulp';
 import {Server} from 'karma';
+import path from 'path';
 import config from '../config';
 
 gulp.task('unit', ['views'], (done) => {
   new Server({
-    configFile: config.test.karma,
+    configFile: path.resolve(config.test.karma),
     singleRun: true
   }, done).start();
 });
