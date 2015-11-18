5654df570aa75436ff62df5ce2ef530179cec995
centsent <ivan.hui.g+d@gmail.com>
2015-11-18 19:01:59 +0800

    fix: fix time point position

--- src/scripts/directives/controls/time/hyTimePoint.js
+++ src/scripts/directives/controls/time/hyTimePoint.js
@@ -10,11 +10,15 @@ function hyTimePoint($hyMedia) {
   return {
     restrict: 'A',
     link($scope, elem) {
+      const width = elem[0].clientWidth;
+      const parentWidth = elem.parent()[0].clientWidth;
+
       elem.bind('click', event => event.stopPropagation());
 
-      $hyMedia.onTimeUpdate((currentTime) => {
-        const percentTime = currentTime / $hyMedia.totalTime * 100;
-        elem.css('left', `${percentTime}%`);
+      $hyMedia.onTimeUpdate(currentTime => {
+        const totalTime = $hyMedia.totalTime;
+        const percentLeft = currentTime / (totalTime + totalTime / parentWidth * width) * 100;
+        elem.css('left', `${percentLeft}%`);
       });
     }
   };
