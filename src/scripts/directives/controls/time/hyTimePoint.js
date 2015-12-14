c9cb2a34b837eabfb2ee65dbc7377ab261bb27ad
centsent <ivan.hui.g+d@gmail.com>
2015-12-14 12:12:15 +0800

    refactor: update directive in es6 class

--- src/scripts/directives/controls/time/hyTimePoint.js
+++ src/scripts/directives/controls/time/hyTimePoint.js
@@ -12,15 +12,23 @@ function hyTimePoint($hyMedia) {
     link($scope, elem) {
       const width = elem[0].clientWidth;
       const parentWidth = elem.parent()[0].clientWidth;
+      let drag = false;
 
-      elem.bind('click', event => event.stopPropagation());
-
-      $hyMedia.onTimeUpdate(currentTime => {
+      $hyMedia.onTimeUpdate((currentTime) => {
         const totalTime = $hyMedia.totalTime;
         const extraTotal = totalTime + totalTime / parentWidth * width;
         const percentLeft = currentTime / extraTotal * 100;
         elem.css('left', `${percentLeft}%`);
       });
+
+      elem.bind('click', (event) => event.stopPropagation())
+        .bind('mousedown', (event) => drag = true)
+        .bind('mousemove', (event) => {
+          if (drag) {
+            console.log(event);
+          }
+        })
+        .bind('mouseup', (event) => drag = false);
     }
   };
 }
