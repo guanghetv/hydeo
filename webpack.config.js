a4a2938449b12ae7edc36f35a46802f1026e3bd4
centsent <ivan.hui.g+d@gmail.com>
2016-01-06 19:19:16 +0800

    chore: update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
-/**
- * Webpack config for development
- */
-module.exports = require('./webpack.make')({
-  BUILD: false,
-  TEST: false,
-});
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH + '/scripts/main.js',
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Hello World app',
+    }),
+  ],
+};
