1e30cd13e74ca2df11b2143966affc1a3da93e34
centsent <ivan.hui.g+d@gmail.com>
2016-01-23 14:20:46 +0800

    refactor: fix slider dragging

--- src/scripts/directives/controls/slider/hySlider.js
+++ src/scripts/directives/controls/slider/hySlider.js
@@ -4,29 +4,11 @@ function hySlider($hyMedia) {
   return {
     restrict: 'A',
     link($scope, elem) {
-      // let dragging = false;
-
-      elem
-        .on('click', (event) => {
-          const width = elem[0].offsetWidth;
-          const time = event.offsetX / width * $hyMedia.totalTime;
-          $hyMedia.seek(time);
-        });
-        // .on('mousedown', () => dragging = true)
-        // .on('mouseup mouseleave', () => {
-          // if (dragging) {
-            // $hyMedia.play();
-          // }
-          // dragging = false;
-        // })
-        // .on('mousemove', (event) => {
-          // if (dragging) {
-            // const width = elem[0].offsetWidth;
-            // const time = event.offsetX / width * $hyMedia.totalTime;
-            // $hyMedia.seek(time);
-            // $hyMedia.pause();
-          // }
-        // });
+      elem.on('click', (event) => {
+        const width = elem[0].offsetWidth;
+        const time = event.offsetX / width * $hyMedia.totalTime;
+        $hyMedia.seek(time);
+      });
     },
   };
 }
--- src/scripts/directives/controls/time/hyTimePoint.js
+++ src/scripts/directives/controls/time/hyTimePoint.js
@@ -3,7 +3,11 @@ function hyTimePoint($hyMedia) {
 
   return {
     restrict: 'A',
+    required: 'hySlider',
     link($scope, elem) {
+      const slider = elem.parent();
+      let dragging = false;
+
       function setTimePointPosition() {
         const width = elem[0].clientWidth;
         const parentWidth = elem.parent()[0].clientWidth;
@@ -17,6 +21,23 @@ function hyTimePoint($hyMedia) {
       $hyMedia.onFullScreenChange(setTimePointPosition);
 
       elem.on('click mousemove', (event) => event.stopPropagation());
+      slider
+        .on('mousedown', () => dragging = true)
+        .on('mouseup mouseleave', () => {
+          if (dragging) {
+            $hyMedia.play();
+          }
+          dragging = false;
+        })
+        .on('mousemove', (event) => {
+          if (dragging) {
+            const width = slider[0].offsetWidth;
+            const time = event.offsetX / width * $hyMedia.totalTime;
+            $hyMedia.pause();
+            setTimePointPosition();
+            $hyMedia.seek(time);
+          }
+        });
     },
   };
 }
