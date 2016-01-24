819bdd6da1e4106d7609a90062913e7f45059a40
centsent <ivan.hui.g+d@gmail.com>
2016-01-24 23:26:04 +0800

    refactor: fix slider dragging issue

--- src/scripts/directives/controls/time/hyTimePoint.js
+++ src/scripts/directives/controls/time/hyTimePoint.js
@@ -5,39 +5,41 @@ function hyTimePoint($hyMedia) {
     restrict: 'A',
     required: 'hySlider',
     link($scope, elem) {
-      const slider = elem.parent();
+      const slider = document.querySelectorAll('[hy-slider]')[0];
       let dragging = false;
 
       function setTimePointPosition() {
         const width = elem[0].clientWidth;
-        const parentWidth = elem.parent()[0].clientWidth;
+        const parentWidth = slider.clientWidth;
         const totalTime = $hyMedia.totalTime;
         const extraTotal = totalTime + totalTime / parentWidth * width;
         const percentLeft = $hyMedia.currentTime / extraTotal * 100;
         elem.css('left', `${percentLeft}%`);
       }
 
+      function disableSliderDragging() {
+        if (dragging) {
+          $hyMedia.play();
+        }
+        dragging = false;
+      }
+
       $hyMedia.onTimeUpdate(setTimePointPosition);
       $hyMedia.onFullScreenChange(setTimePointPosition);
 
       elem.on('click mousemove', (event) => event.stopPropagation());
-      slider
-        .on('mousedown', () => dragging = true)
-        .on('mouseup mouseleave', () => {
-          if (dragging) {
-            $hyMedia.play();
-          }
-          dragging = false;
-        })
-        .on('mousemove', (event) => {
-          if (dragging) {
-            const width = slider[0].offsetWidth;
-            const time = event.offsetX / width * $hyMedia.totalTime;
-            $hyMedia.pause();
-            setTimePointPosition();
-            $hyMedia.seek(time);
-          }
-        });
+      slider.addEventListener('mousedown', () => dragging = true);
+      slider.addEventListener('mouseup', disableSliderDragging);
+      slider.addEventListener('mouseleave', disableSliderDragging);
+      slider.addEventListener('mousemove', (event) => {
+        if (dragging) {
+          const width = slider.offsetWidth;
+          const time = event.offsetX / width * $hyMedia.totalTime;
+          $hyMedia.pause();
+          setTimePointPosition();
+          $hyMedia.seek(time);
+        }
+      });
     },
   };
 }
