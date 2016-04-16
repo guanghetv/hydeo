17ebb1caaf0f8489c6eb753c4d46b7b97cffb27f
centsent <ivan.hui.g+d@gmail.com>
2016-04-16 23:36:13 +0800

    feat: add video props

--- src/components/Hydeo.jsx
+++ src/components/Hydeo.jsx
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default class Hydeo extends Component {
 
   constructor(props, ...args) {
-    super(props, args);
+    super(props, ...args);
     this.state = {};
   }
 
   render() {
     return (
-      <div>Hydeo</div>
+      <div>
+        <video {...this.props} />
+      </div>
     );
   }
 }
+
+Hydeo.propTypes = {
+  src: PropTypes.string,
+  autoPlay: PropTypes.bool,
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-  <Hydeo />,
+  <Hydeo src="http://7xaw4c.com2.z0.glb.qiniucdn.com/pcM_5669d3054ca048700704db9a.mp4" autoPlay />,
   document.getElementById('hydeo')
 );
