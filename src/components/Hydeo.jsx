747301b6ea85715ccb3cb2fe526d6f32ce964368
centsent <ivan.hui.g+d@gmail.com>
2016-04-17 20:30:13 +0800

    feat: add hls support

--- src/components/Hydeo.jsx
+++ src/components/Hydeo.jsx
@@ -1,4 +1,7 @@
 import React, { Component, PropTypes } from 'react';
+import Hls from 'hls.js';
+
+const HLS_FORMAT = '.m3u8';
 
 export default class Hydeo extends Component {
 
@@ -7,16 +10,26 @@ export default class Hydeo extends Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    if (this.props.src.endsWith(HLS_FORMAT) && Hls.isSupported()) {
+      const video = this.refs.media;
+      const hls = new Hls();
+      hls.loadSource(this.props.src);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
+    }
+  }
+
   render() {
     return (
       <div>
-        <video {...this.props} />
+        <video ref="media" {...this.props} />
       </div>
     );
   }
 }
 
 Hydeo.propTypes = {
-  src: PropTypes.string,
+  src: PropTypes.string.isRequired,
   autoPlay: PropTypes.bool,
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-  <Hydeo src="http://7xaw4c.com2.z0.glb.qiniucdn.com/pcM_5669d3054ca048700704db9a.mp4" autoPlay />,
+  <Hydeo src="http://pchls.media.yangcong345.com/pcL_566989c0c41b293c7f4a04c7.m3u8" autoPlay />,
   document.getElementById('hydeo')
 );
