bda541c0cb358c55a6d7df38d0216b9597206fc2
centsent <ivan.hui.g+d@gmail.com>
2016-04-25 07:07:51 +0800

    refactor: change Hydeo component to pass props to children

--- src/components/Hydeo.jsx
+++ src/components/Hydeo.jsx
@@ -1,6 +1,9 @@
 import React, { Component, Children, cloneElement } from 'react';
 import { propTypes, defaultProps } from '../props';
 import Hls from 'hls.js';
+import throttle from 'lodash.throttle';
+import FullScreenApi from '../utils/FullScreenApi';
+import isFunction from '../utils';
 
 const AUDIO_EXTENSIONS = /\.(mp3|wav)($|\?)/;
 const HLS_EXTENSIONS = /\.(m3u8)($|\?)/;
@@ -30,6 +33,7 @@ const EVENTS = [
   'onVolumeChange',
   'onWaiting',
 ];
+const STATE_FRESH_INTERVAL = 300;
 
 export default class Hydeo extends Component {
 
@@ -42,15 +46,22 @@ export default class Hydeo extends Component {
   }
 
   componentWillMount() {
+    this.updateState = throttle(this.updateState, STATE_FRESH_INTERVAL).bind(this);
+
     this.mediaEventProps = EVENTS.reduce((eventMap, current) => {
       const eventProps = eventMap;
-      eventProps[current] = this[current];
+      eventProps[current] = () => {
+        if (current in this.props && isFunction(this.props[current])) {
+          this.props[current].call();
+        }
+        this.updateState();
+      };
       return eventProps;
     }, {});
   }
 
   componentDidMount() {
-    const media = this.mediaElement;
+    const media = this.refs.media;
     if (HLS_EXTENSIONS.test(this.props.src) && Hls.isSupported()) {
       const hls = new Hls();
       hls.loadSource(this.props.src);
@@ -59,17 +70,106 @@ export default class Hydeo extends Component {
     }
   }
 
+  setVolume(volume) {
+    this.refs.media.volume = volume;
+  }
+
+  play() {
+    this.refs.media.play();
+  }
+
+  pause() {
+    this.refs.media.pause();
+  }
+
+  togglePlay() {
+    if (this.state.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+
+  mute() {
+    this.refs.media.muted = true;
+  }
+
+  unmute() {
+    this.refs.media.muted = false;
+  }
+
+  toggleMute() {
+    if (this.state.muted) {
+      this.unmute();
+    } else {
+      this.mute();
+    }
+  }
+
+  seek(time) {
+    this.refs.media.currentTime = time;
+  }
+
+  requestFullScreen() {
+    FullScreenApi.request(this.refs.hydeo);
+    this.setState({ isFullScreen: true });
+  }
+
+  exitFullScreen() {
+    FullScreenApi.exit();
+    this.setState({ isFullScreen: false });
+  }
+
+  toggleFullScreen() {
+    if (this.state.isFullScreen) {
+      this.exitFullScreen();
+    } else {
+      this.requestFullScreen();
+    }
+  }
+
+  updateState() {
+    const media = this.refs.media;
+    const duration = media.duration;
+    const currentTime = media.currentTime;
+    const buffered = media.buffered;
+
+    this.setState({
+      totalTime: duration,
+      currentTime,
+      buffered,
+      paused: media.paused,
+      muted: media.muted,
+      volume: media.volume,
+      isFullScreen: FullScreenApi.isFullScreen,
+      percentageBuffered: buffered.length && buffered.end(buffered.length - 1) / duration * 100,
+      percentagePlayed: currentTime / duration * 100,
+    });
+  }
+
   renderChildren() {
-    return Children.map(this.props.children, (child, ref) => cloneElement(child, { ref }));
+    const extendedProps = Object.assign({
+      play: this.play,
+      pause: this.pause,
+      togglePlay: this.togglePlay,
+      mute: this.mute,
+      unmute: this.unmute,
+      setVolume: this.setVolume,
+      toggleVolume: this.toggleVolume,
+      requestFullScreen: this.requestFullScreen,
+      exitFullScreen: this.exitFullScreen,
+      toggleFullScreen: this.toggleFullScreen,
+    }, this.state);
+
+    return Children.map(this.props.children, (child) => cloneElement(child, extendedProps));
   }
 
   render() {
     const Media = AUDIO_EXTENSIONS.test(this.props.src) ? 'audio' : 'video';
 
-
     return (
-      <div>
-        <Media ref={ (el) => (this.mediaElement = el) } {...this.props} />
+      <div refs="hydeo">
+        <Media ref="media" {...this.props} { ...this.mediaEventProps } />
         { this.renderChildren() }
       </div>
     );
